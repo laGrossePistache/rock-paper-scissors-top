313c2d03c22b3c346f71f35f9b90093778cf5576
Tommy Melançon-Roy <tmelanconroy@gmail.com>
2023-10-22 01:44:56 -0400

    switch to sveltekit. Add home page.

---
+++ src/lib/game.svelte
@@ -0,0 +1,40 @@
+<script>
+	let currentRound = 1;
+	let playerScore = 0;
+	let computerScore = 0;
+</script>
+
+<div id="game-frame">
+	<h2>Round {currentRound}</h2>
+	<p>Player: {playerScore} - Computer: {computerScore}</p>
+</div>
+
+<style>
+	* {
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+	}
+
+	#game-frame {
+		background-color: #176b87;
+        padding: 12px;
+		width: 800px;
+		height: 600px;
+		border-radius: 12%;
+		box-shadow: 10px 5px 5px #072028;
+		border: 2px solid black;
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+	}
+
+	h2 {
+		color: #64ccc5;
+        margin-bottom: 24px;
+	}
+
+    p{
+        color: #DAFFFB
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Game from "../lib/game.svelte";
+
+</script>
+<div id="main-container">
+	<h1>Rock Paper Scissors</h1>
+    <Game/>
+</div>
+
+<style>
+	* {
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+	}
+
+	:root {
+		background-color: #04364a;
+	}
+
+    #main-container{
+        padding: 24px 12px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 24px;
+    }
+
+	h1 {
+		color: #64CCC5;
+	}
+</style>
