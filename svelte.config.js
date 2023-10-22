9e6bd3f4681baeafba0e150403ab3d5c56b20cd7
Tommy Melançon-Roy <tmelanconroy@gmail.com>
2023-10-22 02:53:12 -0400

    change things to make website works with github

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,17 @@
-import adapter from '@sveltejs/adapter-static';
+// svelte.config.js
+import adapter from 'svelte-adapter-github';
 
 export default {
-	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
-	}
+  kit: {
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'docs',
+      assets: 'docs',
+      fallback: null,
+      precompress: false,
+      domain: '',
+      jekyll: false
+    })
+  }
 };
