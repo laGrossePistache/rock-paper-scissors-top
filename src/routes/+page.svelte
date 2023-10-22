6012d799ff314d90d3031820d7fed29ae51b2b82
Tommy Melançon-Roy <tmelanconroy@gmail.com>
2023-10-22 02:41:43 -0400

    build page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script>
-	import Game from "../lib/game.svelte";
-
+	import Game from '../lib/game.svelte';
 </script>
+
 <div id="main-container">
 	<h1>Rock Paper Scissors</h1>
-    <Game/>
+	<Game />
 </div>
 
 <style>
@@ -18,15 +18,15 @@
 		background-color: #04364a;
 	}
 
-    #main-container{
-        padding: 24px 12px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        gap: 24px;
-    }
+	#main-container {
+		padding: 24px 12px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 24px;
+	}
 
 	h1 {
-		color: #64CCC5;
+		color: #64ccc5;
 	}
 </style>
